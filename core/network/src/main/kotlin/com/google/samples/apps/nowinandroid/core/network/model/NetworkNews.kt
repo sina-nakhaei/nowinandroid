7fc4bd32b9dd1b087ae99100a5725ae5af0bec0c
Sina Nakhaei <cna.nakhaei@gmail.com>
2026-09-01 01:20:58 +0330

    feat(news): map network news to domain models

--- core/network/src/main/kotlin/com/google/samples/apps/nowinandroid/core/network/model/NetworkNews.kt
+++ core/network/src/main/kotlin/com/google/samples/apps/nowinandroid/core/network/model/NetworkNews.kt
@@ -1,5 +1,7 @@
 package com.google.samples.apps.nowinandroid.core.network.model
 
+import com.google.samples.apps.nowinandroid.core.model.data.News
+import com.google.samples.apps.nowinandroid.core.model.data.NewsCategory
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -47,3 +49,26 @@ data class NetworkNewsFeed(
     @SerialName("World")
     val world: List<NetworkNews> = emptyList(),
 )
+
+fun NetworkNews.asExternalModel(category: NewsCategory): News =
+    News(
+        id = link,
+        title = title,
+        link = link,
+        imageUrl = imageUrl,
+        source = source,
+        sourceIconUrl = sourceIconUrl,
+        category = category,
+    )
+
+fun NetworkNewsFeed.asExternalModels(): List<News> =
+    buildList {
+        business.forEach { add(it.asExternalModel(NewsCategory.BUSINESS)) }
+        entertainment.forEach { add(it.asExternalModel(NewsCategory.ENTERTAINMENT)) }
+        health.forEach { add(it.asExternalModel(NewsCategory.HEALTH)) }
+        science.forEach { add(it.asExternalModel(NewsCategory.SCIENCE)) }
+        sports.forEach { add(it.asExternalModel(NewsCategory.SPORTS)) }
+        technology.forEach { add(it.asExternalModel(NewsCategory.TECHNOLOGY)) }
+        us.forEach { add(it.asExternalModel(NewsCategory.US)) }
+        world.forEach { add(it.asExternalModel(NewsCategory.WORLD)) }
+    }
